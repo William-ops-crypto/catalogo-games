8fca0f06287e8ecd279725dd2aada6b38d2874f3
William-ops-crypto <william.s.91@hotmail.com>
2023-05-31 12:12:05 -0300

    Debug

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-   // baseURL:"https://dslistbackend-production.up.railway.app/games"
-   baseURL:"http://localhost:8080/buscar"
+    baseURL:"https://dslistbackend-production.up.railway.app/buscar"
+  // baseURL:"http://localhost:8080/buscar"
    //baseURL:"http://localhost:8080/games"
 });
 
